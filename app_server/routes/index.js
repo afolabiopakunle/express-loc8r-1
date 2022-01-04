bdbcb064b5a96ae7134601205a3ae0b11db5dfec
Afolabi Opakunle <opakunleafolabi@gmail.com>
2022-01-04 22:43:21 +0100

    reset project

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const ctrlMain = require('../controllers/main')
+const ctrlMain = require('../controllers/main');
+const ctrlLocation = require('../controllers/locations');
+const ctrlOthers = require('../controllers/others');
+
 /* GET home page. */
 router.get('/', ctrlMain.index);
 
